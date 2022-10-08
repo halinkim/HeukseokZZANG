b9a66e3d488f05f465e86a8ce169a1a275ca774c
halinkim <halin1028@naver.com>
2022-10-08 09:21:59 +0900

    Update basic.cpp

--- codes/basic.cpp
+++ codes/basic.cpp
@@ -19,5 +19,9 @@ using vStr = vector<string>;
 int main(){
 	ios::sync_with_stdio(0);
 	cin.tie(0);
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+//	sys.stdin = open("input.txt", "r")
+//	sys.stdout = open("output.txt", "w")
 
 }
