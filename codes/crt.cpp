005e4469afae7340cad11adf808bc1971f36c467
halinkim <halin1028@naver.com>
2022-11-17 01:01:09 +0900

    edit crt

--- codes/crt.cpp
+++ codes/crt.cpp
@@ -1,5 +1,5 @@
 int CRT (int a1 , int m1 , int a2 , int m2) {
-	return (a1 ? a2 % m1 + m1) * (ll) rev(m2, m1) % m1 * m2 + a2 ;
+	return (a1 - a2 % m1 + m1) * (ll) rev(m2, m1) % m1 * m2 + a2 ;
 }
 
 int rev (int x, int m) {
